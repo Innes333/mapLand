325a92ec15b610bf4165e71604f79ec9f3872f56
MPadalko <mpadalko@mpadalko-mac.kiev.luxoft.com>
2017-10-25 17:53:36 +0300

    progress bar custom values

--- app/js/main.js
+++ app/js/main.js
@@ -3,23 +3,42 @@
 	$(document).ready(function() {
 
 		$('.svg-progress-1').svgprogress({
-			figure: "hexagon",
-			progressFillGradient: ['#fcbf02','#2cbc99'],
-			progressWidth: 8
+			figure: "circle",
+			progressFillGradient: ['#719fdd','#9fe1d5'],
+			progressWidth: 20,
+			unitsOutput: '',
+			endFill: 20
 		}); 
 
 		$('.svg-progress-2').svgprogress({
-			figure: "hexagon",
-			progressFillGradient: ['#fcbf02','#2cbc99'],
-			progressWidth: 8
+			figure: "circle",
+			progressFillGradient: ['#719fdd','#9fe1d5'],
+			progressWidth: 20,
+			unitsOutput: '',
+			endFill: 75
 		}); 
 
 		$('.svg-progress-3').svgprogress({
-			figure: "hexagon",
-			progressFillGradient: ['#fcbf02','#2cbc99'],
-			progressWidth: 8
-		}); 
+			figure: "circle",
+			progressFillGradient: ['#719fdd','#9fe1d5'],
+			progressWidth: 20,
+			unitsOutput: '',
+			endFill: 50
+		});
 
+	});
+
+	$(window).scroll(function() {
+		
+		var obj = $('.w-progress').position().top,
+			scrollTop = $(window).scrollTop();
+		
+		if (scrollTop === obj) {
+			$('.svg-progress-1').trigger("redraw");
+			$('.svg-progress-2').trigger("redraw");
+			$('.svg-progress-3').trigger("redraw");
+		}
+		
 	});
 
 })();
